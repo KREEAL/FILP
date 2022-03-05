6805f67f99a050017dc90a136639c7100bae3e5a
Цветков Киллриал <kiryatsvetkov@mail.ru>
2022-03-05 16:27:41 +0300

    Solved 1

--- FILPfsharp/Program.fs
+++ FILPfsharp/Program.fs
@@ -2,7 +2,17 @@
 
 open System
 
+let ask s =
+    if (s = "F#" || s = "Prolog") then "Ты подлиза!"
+    else 
+        let s1 = s + " прекрасный язык))"
+        s1
+        
+
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
+    Console.WriteLine("Какой у тебя любимый язык программирования?");
+    let s = Console.ReadLine();
+    Console.WriteLine (ask s)
+    Console.ReadKey();
     0 // return an integer exit code
