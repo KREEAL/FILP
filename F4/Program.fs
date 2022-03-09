80b50eae52da308ac32c1597c205662cb44afb7f
Цветков Киллриал <kiryatsvetkov@mail.ru>
2022-03-09 21:20:22 +0300

    Solved F4

--- F4/Program.fs
+++ F4/Program.fs
@@ -2,7 +2,21 @@
 
 open System
 
+let f n predicate init =
+    let rec f_inside x func init divider =
+        if divider = 1 then init
+        else
+            let newInit = 
+                if x % divider = 0 then 
+                    func init divider 
+                else init
+            let newDivider = divider - 1
+            f_inside x func newInit newDivider
+    f_inside n predicate init (n/2)
+    
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
+
+    System.Console.WriteLine(f 12 (fun x y -> x + y) 0)
+
     0 // return an integer exit code
