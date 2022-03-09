b68d23ebf118e03d614bc4dc9ed5fd8006ba45d1
Цветков Киллриал <kiryatsvetkov@mail.ru>
2022-03-09 19:37:30 +0300

    LR2 fixed?

--- F2/Program.fs
+++ F2/Program.fs
@@ -11,12 +11,25 @@ let ask s =
 
 [<EntryPoint>]
 let main argv =
+    let lowS (s:String) = s.ToLower()
+    let answer_s s = 
+        match s with
+        |"F#"|"Prolog"->"Подлиза"
+        |_->"прекрасный язык"
+
+    //каррирование
+    Console.WriteLine("Какой у тебя любимый язык программирования?");
+    (Console.ReadLine>> lowS >> answer_s>> Console.WriteLine)()
+
+    //суперпозиция
+    Console.WriteLine("Какой у тебя любимый язык программирования?");
+    let user input (output:string->unit) chooser = output (chooser (input ()))
+    user Console.ReadLine Console.WriteLine answer_s
+
+    //конвеер
     Console.WriteLine("Какой у тебя любимый язык программирования?");
     Console.ReadLine()|> ask |> Console.WriteLine
     Console.ReadKey();
 
-    Console.WriteLine("Какой у тебя любимый язык программирования?");
-    let s = Console.ReadLine();
-    Console.WriteLine (ask s)
-    Console.ReadKey();
+
     0 // return an integer exit code
