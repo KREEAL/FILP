569e797a3da49f2400705e01f50e673e79a90e60
Цветков Киллриал <kiryatsvetkov@mail.ru>
2022-03-10 17:38:44 +0300

    F8.2

--- F8/Program.fs
+++ F8/Program.fs
@@ -30,8 +30,26 @@ let maxEasyDivider n =
             maxED_inside n newMax newCounter
     maxED_inside n 1 n
 
+let notDivisibleBy5DigitsApp n = 
+    let rec notDivisibleBy5DigitsApp_inside n res founded= 
+        if (n=0) then
+            if founded = 0 then 0
+            else res
+        else 
+            let cifr = n % 10
+            let n1 = n / 10
+            let acc = (fun x y -> if y % 5 = 0 then x else x*y) res cifr
+            let newFounded = 
+                if cifr % 5 <> 0 then founded + 1
+                else founded                
+            notDivisibleBy5DigitsApp_inside n1 acc newFounded
+    notDivisibleBy5DigitsApp_inside n 1 0
+
 [<EntryPoint>]
 let main argv =
-    maxEasyDivider 20 |>Console.Write
-    maxEasyDivider 19 |>Console.Write
+    maxEasyDivider 20 |>Console.WriteLine
+    maxEasyDivider 19 |>Console.WriteLine
+    notDivisibleBy5DigitsApp 12534 |> Console.WriteLine
+    notDivisibleBy5DigitsApp 555 |> Console.WriteLine
+    notDivisibleBy5DigitsApp 0 |> Console.WriteLine
     0 // return an integer exit code
