ee43263bef659a44c42c44ab45eae9e9cdf37ecb
Цветков Киллриал <kiryatsvetkov@mail.ru>
2022-03-10 18:09:24 +0300

    F8.3

--- F8/Program.fs
+++ F8/Program.fs
@@ -15,7 +15,7 @@ let isEasy n =
                 isEasy_inside n newCur
     isEasy_inside n (n-1)
 
-
+    //максимальный простой делитель
 let maxEasyDivider n =
     let rec maxED_inside n maxD counter =
         if counter = 0 then 0
@@ -30,6 +30,8 @@ let maxEasyDivider n =
             maxED_inside n newMax newCounter
     maxED_inside n 1 n
 
+
+    //произведение цифр не делящихся на 5
 let notDivisibleBy5DigitsApp n = 
     let rec notDivisibleBy5DigitsApp_inside n res founded= 
         if (n=0) then
@@ -45,6 +47,49 @@ let notDivisibleBy5DigitsApp n =
             notDivisibleBy5DigitsApp_inside n1 acc newFounded
     notDivisibleBy5DigitsApp_inside n 1 0
 
+
+    //произведение цифр
+let digitsApply n =
+    if n = 0 then 0
+    else
+        let rec digitsA_inside n res =
+            if n = 0 then res
+            else
+                let cifr = n%10
+                let n1 = n/10
+                let acc = res*cifr
+                digitsA_inside n1 acc
+        digitsA_inside n 1
+        
+// НОД по Евклиду
+let rec GCD_Euqlid x y =
+    if ((x = 0 ) || (y = 0)) then
+        (x + y)
+    else
+        if x > y then GCD_Euqlid (x % y) y
+        else GCD_Euqlid x (y % x)
+
+//максимальный не простой нечетный делитель
+let maxNotEasyOddDivider n =
+    let rec maxED_inside n maxD counter =
+        if counter = 0 then 0
+        else 
+        if counter = 1 then maxD
+        else
+            let newMax =
+                if (isEasy counter = 0) && (n % counter = 0) && (counter > maxD) && (counter % 2<>0) then
+                    counter
+                else maxD
+            let newCounter = counter - 1
+            maxED_inside n newMax newCounter
+    maxED_inside n 1 n
+
+//...
+let GCD_maxNEOD_DigApp n =
+    GCD_Euqlid (maxNotEasyOddDivider n) (digitsApply n)
+
+
+
 [<EntryPoint>]
 let main argv =
     maxEasyDivider 20 |>Console.WriteLine
@@ -52,4 +97,6 @@ let main argv =
     notDivisibleBy5DigitsApp 12534 |> Console.WriteLine
     notDivisibleBy5DigitsApp 555 |> Console.WriteLine
     notDivisibleBy5DigitsApp 0 |> Console.WriteLine
+
+    GCD_maxNEOD_DigApp 45 |>Console.WriteLine //НОД 15 20 = 5 
     0 // return an integer exit code
