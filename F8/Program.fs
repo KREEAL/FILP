f4644e28a96ccd6c99d51b5ad9d5433af054f1a6
Цветков Киллриал <kiryatsvetkov@mail.ru>
2022-03-10 17:17:34 +0300

    F8.1

--- F8/Program.fs
+++ F8/Program.fs
@@ -2,7 +2,36 @@
 
 open System
 
+//простое или нет
+let isEasy n =
+    let rec isEasy_inside n cur=
+        if cur = 0 then 0
+        else 
+        if cur = 1 then 1
+        else
+            if n % cur = 0 then 0
+            else 
+                let newCur = cur - 1
+                isEasy_inside n newCur
+    isEasy_inside n (n-1)
+
+
+let maxEasyDivider n =
+    let rec maxED_inside n maxD counter =
+        if counter = 0 then 0
+        else 
+        if counter = 1 then maxD
+        else
+            let newMax =
+                if (isEasy counter = 1) && (n % counter = 0) && (counter > maxD) then
+                    counter
+                else maxD
+            let newCounter = counter - 1
+            maxED_inside n newMax newCounter
+    maxED_inside n 1 n
+
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
+    maxEasyDivider 20 |>Console.Write
+    maxEasyDivider 19 |>Console.Write
     0 // return an integer exit code
