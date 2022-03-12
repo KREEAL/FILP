1cb3d60a01ccd90aee1a657ec1169d8f68dc1ecf
Цветков Киллриал <kiryatsvetkov@mail.ru>
2022-03-12 19:38:24 +0300

    Solved F2. Долго не было интернета.

--- Lab6/Lab6/F2/Program.fs
+++ Lab6/Lab6/F2/Program.fs
@@ -2,6 +2,8 @@
 
 open System
 
+let max2 x y = if x > y then x else y
+
 let rec readList n =
     if n = 0 then []
     else
@@ -25,10 +27,32 @@ let rec getByIndex list i =
             else
                 getByIndex t (i-1)
 
+
+let rec listElements list (f : int -> int -> int) p acc = 
+    match list with
+    | [] -> acc
+    | head::tail ->
+                let newAcc = f acc head
+                if p head then listElements tail f p newAcc
+                else listElements tail f p acc
+
+let listMax list = 
+    match list with 
+    |[] -> 0
+    | h::t -> listElements list max2 (fun x -> true) h
+
+let isIndexedElMax list index =
+    if listMax list = getByIndex list index then true
+    else false
+
 [<EntryPoint>]
 let main argv =
-    
-    
-
-    Console.Write(getByIndex (readList 5) 3 )
+    let n = Console.ReadLine() |> Int32.Parse
+    let index = Console.ReadLine() |> Int32.Parse
+    let list = readList n
+    let result = isIndexedElMax list index
+    match result with
+    |true ->Console.WriteLine "Является"
+    |false ->Console.WriteLine "Не является"
+       
     0 // return an integer exit code
