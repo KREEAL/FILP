09439871eeed74dbcb2015faaa53eb165d4f0947
Цветков Киллриал <kiryatsvetkov@mail.ru>
2022-03-13 16:34:19 +0300

    Solved F5

--- Lab6/Lab6/F5/Program.fs
+++ Lab6/Lab6/F5/Program.fs
@@ -2,7 +2,38 @@
 
 open System
 
+let rec writeList = function
+    [] ->   let z = System.Console.ReadKey()
+            0
+    | (head : int)::tail -> 
+                       System.Console.WriteLine(head)
+                       writeList tail
+
+let rec readList n =
+    if n = 0 then []
+    else
+    let Head = Console.ReadLine()|>Convert.ToInt32
+    let Tail = readList (n-1)
+    Head::Tail
+
+
+let moveLeft list =
+    let rec moveLeft_inside list head count =
+        if count > 0 then
+            list @ [head]
+        else
+        match list with
+        |[] -> list //никогда не будет обработано
+        |h::t ->
+            let newCount = count + 1
+            moveLeft_inside t h newCount 
+    moveLeft_inside list 0 0
+
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
-    0 // return an integer exit code
+    //циклический сдвиг влево 
+    let n = Console.ReadLine() |> Convert.ToInt32
+    let list = readList n
+    writeList (moveLeft list)
+     // return an integer exit code
+        
