ae19ac4e46f6ff46affc7973577ce26580816657
Цветков Киллриал <kiryatsvetkov@mail.ru>
2022-03-13 17:07:24 +0300

    Solved F7

--- Lab6/Lab6/F7/Program.fs
+++ Lab6/Lab6/F7/Program.fs
@@ -2,7 +2,36 @@
 
 open System
 
+let rec readList n =
+    if n = 0 then []
+    else
+    let Head = Console.ReadLine()|>Convert.ToInt32
+    let Tail = readList (n-1)
+    Head::Tail
+
+
+let rec writeList = function
+    [] ->   let z = System.Console.ReadKey()
+            0
+    | (head : int)::tail -> 
+                       System.Console.WriteLine(head)
+                       writeList tail
+
+let isEven n = ((n % 2) = 0)
+
+let evenOdd list = 
+    let rec evenOdd_inside list even odd index =
+        match list with
+        |[] -> (even @ odd)
+        |h::t ->
+                let newIndex = index + 1
+                if isEven index then evenOdd_inside t (even @ [h]) odd newIndex
+                else evenOdd_inside t even (odd @ [h]) newIndex
+    evenOdd_inside list [] [] 0
+
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
-    0 // return an integer exit code
+    //вывести сначала элементы с четными индексами, затем с нечетными индексами
+    let n = Console.ReadLine() |> Int32.Parse
+    writeList (evenOdd (readList n))
+    // return an integer exit code
