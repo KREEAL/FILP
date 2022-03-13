6c06cb69a7a347459fe3c5674a78f5425184d4f0
Цветков Киллриал <kiryatsvetkov@mail.ru>
2022-03-13 16:43:42 +0300

    Solved F6. Изменил знаки < на > из F4

--- Lab6/Lab6/F6/Program.fs
+++ Lab6/Lab6/F6/Program.fs
@@ -2,7 +2,52 @@
 
 open System
 
+let rec readList n =
+    if n = 0 then []
+    else
+    let Head = Console.ReadLine()|>Convert.ToInt32
+    let Tail = readList (n-1)
+    Head::Tail
+
+let listSize list =
+    let rec listSize_inside list res = 
+        match list with
+        |[]-> res
+        |h::t ->
+            let newRes = res + 1
+            listSize_inside t newRes 
+    listSize_inside list 0
+
+
+let rec getByIndex list i =
+    match list with 
+    |[]-> -1
+    |h::t ->
+            if i = 0  then h
+            else
+                getByIndex t (i-1)
+
+
+let isLocalMax list i =
+    if listSize list = 1 then true
+    else
+    let lastInd = (listSize list - 1)
+    match i with
+    |0 ->
+        if getByIndex list i > getByIndex list (i + 1) then true
+        else false
+    |lastInd -> 
+        if getByIndex list i > getByIndex list (i - 1) then true
+        else false
+    |_ -> 
+        if getByIndex list i > getByIndex list (i + 1) && getByIndex list i > getByIndex list (i + 1) then true else false
+
+
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
+    //Является ли элемент по указанному индексу локальным минимумом?
+    let n = Console.ReadLine() |> Convert.ToInt32
+    let index = Console.ReadLine() |> Convert.ToInt32
+    let list = readList n
+    Console.WriteLine (isLocalMax list index)
     0 // return an integer exit code
