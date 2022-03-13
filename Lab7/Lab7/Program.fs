e15ceb5e483fb588f1aaaaade4a8acdc8a642bd7
Цветков Киллриал <kiryatsvetkov@mail.ru>
2022-03-13 20:19:57 +0300

    Solved F1

--- Lab7/Lab7/Program.fs
+++ Lab7/Lab7/Program.fs
@@ -2,7 +2,28 @@
 
 open System
 
+let rec readList n = 
+    if n=0 then []
+    else
+    let Head = System.Convert.ToInt32(System.Console.ReadLine())
+    let Tail = readList (n-1)
+    Head::Tail
+
+let readData = 
+    let n=System.Convert.ToInt32(System.Console.ReadLine())
+    readList n
+
 [<EntryPoint>]
 let main argv =
+    
+   (* 1.3 Дан целочисленный массив и натуральный индекс (число, меньшее
+    размера массива). Необходимо определить является ли элемент по
+    указанному индексу глобальным максимумом.*)
+    let list = readData
+    let index = Convert.ToInt32(Console.ReadLine())
+    let maxEl = List.max list
+    let indexedEl = List.item index list 
+    if maxEl = indexedEl then Console.WriteLine "Является"
+    else Console.WriteLine "Не является"
     printfn "Hello World from F#!"
     0 // return an integer exit code
