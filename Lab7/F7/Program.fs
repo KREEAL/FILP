08f2ecbedc894ae86f6ab82f88ed39a0571320e2
Цветков Киллриал <kiryatsvetkov@mail.ru>
2022-03-14 19:17:38 +0300

    Решено, осталось запихнуть в одну функцию, но у меня сейчас электричка приедет, надо на трамвай идти, дома доделаю

--- Lab7/F7/Program.fs
+++ Lab7/F7/Program.fs
@@ -2,7 +2,26 @@
 
 open System
 
+let rec GCD x y = 
+    if x = 0 || y = 0 then x + y
+    else if x > y then GCD (x % y) y else GCD x (y % x)
+
+
+
 [<EntryPoint>]
 let main argv =
+    (*3 Для введенного числа N построить список неповторяющихся кортежей
+    (a,b), таких, что существует пара (x,y): X*Y=N, НОД(X,Y)=d, a=X/d, b=Y/d .*)
+    let n = Console.ReadLine() |> Int32.Parse
+    let l1 = [1..n]
+    let l2 = [1..n]
+    let pairs = List.allPairs l1 l2
+    let XYN = List.filter (fun x -> fst x * snd x = n  ) pairs //x*y=N
+    let NewN = [1..List.length XYN]
+    let Zeros = List.map2 (fun x y -> x - y) NewN NewN 
+    let Nod = List.map2 (fun x y -> GCD(fst x)(snd x) + y) XYN Zeros//список НОД
+    let ResultAB = List.map2(fun x y ->((fst x)/y),((snd x)/y)  )XYN Nod
+
+    Console.Write ResultAB
     printfn "Hello World from F#!"
     0 // return an integer exit code
