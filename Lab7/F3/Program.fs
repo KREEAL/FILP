eafc31dfce4e46ae9fda8184283ea742490bd8f9
Цветков Киллриал <kiryatsvetkov@mail.ru>
2022-03-14 10:23:41 +0300

    Solved F3

--- Lab7/F3/Program.fs
+++ Lab7/F3/Program.fs
@@ -2,7 +2,32 @@
 
 open System
 
+let rec readList n = 
+    if n=0 then []
+    else
+    let Head = System.Convert.ToInt32(System.Console.ReadLine())
+    let Tail = readList (n-1)
+    Head::Tail
+
+let readData = 
+    let n=System.Convert.ToInt32(System.Console.ReadLine())
+    readList n
+
+let rec writeList = function
+    [] ->   let z = System.Console.ReadKey()
+            0
+    | (head : int)::tail -> 
+                       System.Console.WriteLine(head)
+                       writeList tail
+
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
+    (*1.23
+    Дан целочисленный массив. Необходимо найти два наименьших
+    элемента.*)
+    let list = readData
+    let min1 = List.min list
+    let listWithout = List.filter (fun x -> x <> min1) list
+    let min2 = if List.isEmpty listWithout then min1 else List.min listWithout
+    Console.WriteLine("{0} {1}",min1,min2)
     0 // return an integer exit code
