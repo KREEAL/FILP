7bfbfb9f20c2a1da853e536b534d3822c160cc12
Цветков Киллриал <kiryatsvetkov@mail.ru>
2022-03-14 19:19:43 +0300

    Запихнул в функцию, сейчас сделаю ее красивее

--- Lab7/F7/Program.fs
+++ Lab7/F7/Program.fs
@@ -7,12 +7,7 @@ let rec GCD x y =
     else if x > y then GCD (x % y) y else GCD x (y % x)
 
 
-
-[<EntryPoint>]
-let main argv =
-    (*3 Для введенного числа N построить список неповторяющихся кортежей
-    (a,b), таких, что существует пара (x,y): X*Y=N, НОД(X,Y)=d, a=X/d, b=Y/d .*)
-    let n = Console.ReadLine() |> Int32.Parse
+let magicFunction n =
     let l1 = [1..n]
     let l2 = [1..n]
     let pairs = List.allPairs l1 l2
@@ -21,7 +16,13 @@ let main argv =
     let Zeros = List.map2 (fun x y -> x - y) NewN NewN 
     let Nod = List.map2 (fun x y -> GCD(fst x)(snd x) + y) XYN Zeros//список НОД
     let ResultAB = List.map2(fun x y ->((fst x)/y),((snd x)/y)  )XYN Nod
+    ResultAB
 
-    Console.Write ResultAB
+[<EntryPoint>]
+let main argv =
+    (*3 Для введенного числа N построить список неповторяющихся кортежей
+    (a,b), таких, что существует пара (x,y): X*Y=N, НОД(X,Y)=d, a=X/d, b=Y/d .*)
+    let n = Console.ReadLine() |> Int32.Parse
+    Console.Write (magicFunction n)
     printfn "Hello World from F#!"
     0 // return an integer exit code
