58fd2c47bff44afcd873aeff651c05de2446925c
Цветков Киллриал <kiryatsvetkov@mail.ru>
2022-03-14 11:14:43 +0300

    Solved F5

--- Lab7/F4/Program.fs
+++ Lab7/F4/Program.fs
@@ -2,7 +2,37 @@
 
 open System
 
+let rec readList n = 
+    if n=0 then []
+    else
+    let Head = System.Convert.ToInt32(System.Console.ReadLine())
+    let Tail = readList (n-1)
+    Head::Tail
+
+let readData = 
+    let n=System.Convert.ToInt32(System.Console.ReadLine())
+    readList n
+
+let rec writeList = function
+    [] ->   let z = System.Console.ReadKey()
+            0
+    | (head : int)::tail -> 
+                       System.Console.WriteLine(head)
+                       writeList tail
+
+let f5 list = List.length (List.filter (fun x -> (List.exists (fun el -> el * el = x) list)) list)
+
 [<EntryPoint>]
 let main argv =
+    (*1.33
+    Дан целочисленный массив. Проверить, чередуются ли в нем
+    положительные и отрицательные числа.*)
+
+    let list = readData
+    List.exactlyOne
+    List.groupBy
+    List.pairwise
+    List.scan
+    let even = list.[0]
     printfn "Hello World from F#!"
     0 // return an integer exit code
--- Lab7/F5/Program.fs
+++ Lab7/F5/Program.fs
@@ -2,7 +2,36 @@
 
 open System
 
+let rec readList n = 
+    if n=0 then []
+    else
+    let Head = System.Convert.ToInt32(System.Console.ReadLine())
+    let Tail = readList (n-1)
+    Head::Tail
+
+let readData = 
+    let n=System.Convert.ToInt32(System.Console.ReadLine())
+    readList n
+
+let rec writeList = function
+    [] ->   let z = System.Console.ReadKey()
+            0
+    | (head : int)::tail -> 
+                       System.Console.WriteLine(head)
+                       writeList tail
+
+let countMin list = 
+    let minEl = List.min list
+    let result = List.length (List.filter (fun x -> x = minEl) list)
+    result
+
+
 [<EntryPoint>]
 let main argv =
+    (*1.43
+    Дан целочисленный массив. Необходимо найти количество
+    минимальных элементов.*)
+    let list = readData
+    Console.WriteLine(countMin list)
     printfn "Hello World from F#!"
     0 // return an integer exit code
