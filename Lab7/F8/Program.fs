d5aef5482961736a57649e9f867eead3536dedbb
Цветков Киллриал <kiryatsvetkov@mail.ru>
2022-03-14 13:06:47 +0300

    Solved F8

--- Lab7/F8/Program.fs
+++ Lab7/F8/Program.fs
@@ -2,7 +2,31 @@
 
 open System
 
+let readArray n =
+    let rec readArray_inside n arr = 
+        if n=0 then arr
+        else
+        let tail = System.Convert.ToInt32(System.Console.ReadLine())
+        let newArr = Array.append arr [|tail|]
+        let n1 = n - 1
+        readArray_inside n1 newArr
+    readArray_inside n Array.empty
+
+let writeArray arr =
+    printfn "%A" arr
+
+let concatArrays arr1 arr2 = 
+    let newArray = Array.append arr1 arr2
+    newArray
+
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
+    (*3 Объединить два произвольных массива в один.*)
+    let n1 = Console.ReadLine() |>Int32.Parse
+    let firstArray = readArray  n1
+    let n2 = Console.ReadLine() |> Int32.Parse
+    let secondArray = readArray n2
+
+    let concatedArray = concatArrays firstArray secondArray
+    writeArray concatedArray
     0 // return an integer exit code
