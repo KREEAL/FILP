550e1f7e8c95319c28e7fafde78febb5b717544c
Цветков Киллриал <kiryatsvetkov@mail.ru>
2022-03-15 15:59:06 +0300

    Added частота букв в словах, частота алфавита все дела

--- Lab7/F10/Program.fs
+++ Lab7/F10/Program.fs
@@ -17,37 +17,70 @@ let rec writeStringList = function
     | (head : string)::tail -> 
                       System.Console.WriteLine(head)
                       writeStringList tail
+let rec writePairs list =
+    match list with
+    |[] -> 0
+    |_->
+        Console.Write ("({0},{1}) ",fst list.Head,snd list.Head)
+        writePairs list.Tail
 
-let rec writeDoubleList = function
+let rec writeFrequencedList = function
     |[] -> let z = System.Console.ReadKey()
            0
-    | (head : double)::tail -> 
-                      System.Console.WriteLine(head)
-                      writeDoubleList tail
+    | head::tail -> 
+                      writePairs head
+                      Console.WriteLine " "
+                      writeFrequencedList tail
+
+let count x = Seq.filter ((=) x) >> Seq.length
 
 
-let stringFreqList string = 
-    let ss = String.length string
-    let afreq = String.length (String.filter (fun x -> x = 'a')  string) /ss
-    let bfreq = String.length (String.filter (fun x -> x = 'b')  string) /ss
-    let cfreq = String.length (String.filter (fun x -> x = 'c')  string) /ss
-    let dfreq = String.length (String.filter (fun x -> x = 'd')  string) /ss
-    let efreq = String.length (String.filter (fun x -> x = 'e')  string) /ss
-    let (newAplhabet:'double list) = [Convert.ToDouble(afreq);Convert.ToDouble bfreq;Convert.ToDouble cfreq; Convert.ToDouble dfreq; Convert.ToDouble efreq]
-    newAplhabet
 
-[<EntryPoint>]
+//возвращает список кортежей буква-частота для данной строки
+let freqList (s:string) = 
+    let charlist = Seq.toList (s.ToLower())//строка в список чаров
+    let freq = List.countBy id charlist // разбивает на кортеж (буква.кол-во)('а',3)
+    let length = (String.length (String.filter (fun x-> x <> ' ') s)) //без учета пробелов работаем
+    //let length = (String.length s) //с учетом пробелов
+    let newMapList = [0..List.length freq]
+    let symbolFreak = List.map (fun (x:char,x1:int)  -> (x , (Convert.ToDouble x1 ) / (Convert.ToDouble length)) )freq
+    symbolFreak
+
+let difference string frequence alphabet = 
+   
+    
+   [<EntryPoint>]
 let main argv =
-    let alphabetFrequency = [('a',0.1);('b',0.38);('c',0.21);('d',0.19);('e',0.12)]
-    let strings = readStrings 2 []
-    let firstString = strings.Head
-    Console.Write firstString
-    let (freqlist:'double list) = stringFreqList firstString
-    writeDoubleList freqlist
-    writeStringList strings
-    //let (s:string) = "Hello"
-    //let newS = String.filter (fun x -> x <> ' ')
-    //list strings
-    //list frequencies
+    (*3
+    В порядке увеличения разницы между частотой наиболее часто
+    встречаемого символа в строке и частотой его появления в алфавите
+    4
+    В порядке увеличения квадратичного отклонения среднего веса
+    ASCII-кода символа строки от среднего веса ASCII-кода символа первой
+    строки*)
+    (*let alphabetFrequency = [('a',0.1);('b',0.38);('c',0.21);('d',0.19);('e',0.12)])*)
+    
+    let n = Console.ReadLine() |> Int32.Parse
+    let strings = readStrings n []
+    let frequencyList = List.map (fun x -> freqList(x)) strings
+
+    //writeFrequencedList frequencyList
+
+    let bigString = String.concat ("":string) (strings)
+    let bigStringNoSpace = String.filter( fun x -> x <> ' ') bigString
+    let alphabet = freqList bigStringNoSpace
+    writePairs alphabet
+
+    List.sortBy ()
     printfn "Hello World from F#!"
     0 // return an integer exit code
+
+
+(*
+5
+Привет я
+Анатолий Вассерман
+Что бы такого сказать
+Ууууубью сука
+пшел вон
+*)
