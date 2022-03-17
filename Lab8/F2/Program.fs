62b6ede9568b6227404ec241588aeec412cffdee
Цветков Киллриал <kiryatsvetkov@mail.ru>
2022-03-17 15:22:02 +0300

    Добавлен тип данных Maybe, функтор и аппликативный функтор

--- Lab8/F2/Program.fs
+++ Lab8/F2/Program.fs
@@ -2,6 +2,22 @@
 
 open System
 
+type Maybe<'a> =
+    | Just of 'a
+    | Nothing 
+
+//функтор 
+let fmapMaybe f p =
+    match p with
+    | Just a -> Just (f a)
+    | Nothing -> Nothing
+
+//Аппликативный функтор
+let applyMaybe lf p =
+    match lf, p with
+    |Just f, Just a -> Just (f a)
+    |_-> Nothing   
+
 [<EntryPoint>]
 let main argv =
     printfn "Hello World from F#!"
