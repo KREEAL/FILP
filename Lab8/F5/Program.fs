0df09b01e1681dea4793d9a1b1ad1a2cac96ab33
Цветков Киллриал <k-real1337@mail.ru>
2022-03-19 15:42:01 +0300

    Created a driver's license class only with information

---
+++ Lab8/F5/Program.fs
@@ -0,0 +1,18 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+type driversLicense(nam:string, surnam:string, birtDt:DateTime, plac:string, extDt: DateTime, expDt:DateTime,nm:int ) =
+    member this.name:string = nam
+    member this.surname:string = surnam
+    member this.birthDt:DateTime = birtDt
+    member this.place:string = plac
+    member this.extrDT:DateTime = extDt
+    member this.exprDT:DateTime = expDt
+    member this.num:int = nm
+
+
+[<EntryPoint>]
+let main argv =
+    printfn "Hello World from F#!"
+    0 // return an integer exit code
