64d7e8cddd19022cc6655501ad37bae509e9420e
Цветков Киллриал <k-real1337@mail.ru>
2022-03-19 18:23:27 +0300

    Added validation

--- Lab8/F5/Program.fs
+++ Lab8/F5/Program.fs
@@ -1,6 +1,8 @@
 ﻿// Learn more about F# at http://fsharp.org
 
 open System
+open System.Text.RegularExpressions
+
 
 type driversLicense(nam:string, surnam:string, birtDt:DateTime, plac:string, extDt: DateTime, expDt:DateTime,nm:int ) =
     member this.name:string = nam
@@ -17,8 +19,32 @@ let driverLicenseNumEquality (dr1:driversLicense) (dr2:driversLicense) =
     if dr1.num = dr2.num then true
     else false
 
+let (|Regex|) pattern input =
+   let m = Regex.Match(input,pattern)
+   if (m.Success) then Some m.Groups.[1].Value else None
+
+let createDrLWithValidate = 
+    let dtRegex = "^(?:(?:31(\/|-|\.)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\/|-|\.)(?:0?[13-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/|-|\.)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/|-|\.)(?:(?:0?[1-9])|(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$"
+    let strRegex = "@[A-Z][a-z]*"
+    let surname = Console.ReadLine()
+    let name = Console.ReadLine()
+    let (birthdayDT:string) = Console.ReadLine()
+    let (place:string) = Console.ReadLine()
+    let extDate = Console.ReadLine()
+    let expDate = Console.ReadLine()
+    let num = Console.ReadLine()
+       
+    if (Regex.IsMatch(name,strRegex) && Regex.IsMatch(surname,strRegex)
+        && Regex.IsMatch(birthdayDT,dtRegex) && Regex.IsMatch(place,strRegex)&&
+        Regex.IsMatch(extDate,dtRegex)&& Regex.IsMatch(expDate,dtRegex)&& Regex.IsMatch(num,"^[1-9]+[0-9]*$")) then 
+        Some(driversLicense(name,surname,DateTime.Parse birthdayDT,place,DateTime.Parse extDate,DateTime.Parse expDate,Int32.Parse num))
+    else
+        None
+
+
 [<EntryPoint>]
 let main argv =
+
 
     let drL1 = driversLicense("Иван","Иванов",DateTime.Parse "01.01.1991","г.Москва",DateTime.Parse "01.01.2021",DateTime.Parse "01.01.2031",7777777)
     Console.WriteLine drL1
@@ -27,4 +53,9 @@ let main argv =
     match driverLicenseNumEquality drL1 drL2 with
     |true-> Console.WriteLine("Номера документов совпадают")
     |false-> Console.WriteLine("Номера документов не совпадают")
+    
+    let q1 = createDrLWithValidate 
+    match q1 with
+    |driversLicense -> Console.WriteLine "Объет успешно создан"
+    |None->Console.WriteLine "Объект не создан"
     0 // return an integer exit code
