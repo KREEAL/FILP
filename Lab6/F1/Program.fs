69871c3b7bd41d79702860c2b8ae830fc1b4f061
Цветков Киллриал <k-real1337@mail.ru>
2022-03-19 14:21:03 +0300

    КРАСОТА

--- Lab6/F1/Program.fs
+++ Lab6/F1/Program.fs
@@ -1,23 +1,5 @@
 ﻿// Learn more about F# at http://fsharp.org
-
-open System
-
-
-let rec readList n =
-    if n = 0 then []
-    else
-    let Head = Console.ReadLine()|>Convert.ToInt32
-    let Tail = readList (n-1)
-    Head::Tail
-
-
-let rec writeList = function
-    [] ->   let z = System.Console.ReadKey()
-            0
-    | (head : int)::tail -> 
-                       System.Console.WriteLine(head)
-                       writeList tail
-//обертка
+(*//обертка
 let triplet list (f : int -> int -> int -> int) =
     //внутренняя функция. Храним предыдущие значения с голов
     let rec triplet_inside list (f : int -> int -> int -> int) last1 last2 last3 count resList = 
@@ -42,17 +24,51 @@ let triplet list (f : int -> int -> int -> int) =
                         let nowRes = resList @ [f last2 last3 h] 
                         triplet_inside t f 1 1 1 0 nowRes
         
-    triplet_inside list f 1 1 1 0 []
+    triplet_inside list f 1 1 1 0 []*)
     
     //a::t
     //a::b::t
     //a::b::c::d
+open System
+
+
+let rec readList n =
+    if n = 0 then []
+    else
+    let Head = Console.ReadLine()|>Convert.ToInt32
+    let Tail = readList (n-1)
+    Head::Tail
+
+
+let rec writeList = function
+    [] ->   let z = System.Console.ReadKey()
+            0
+    | (head : int)::tail -> 
+                       System.Console.WriteLine(head)
+                       writeList tail
+
+let sum3 a b c = (a + b) + c
+
+let triplet (list:'int list) =
+    let rec triplet_inside list (resList:'int list) =
+        match list with
+        |[] -> resList
+        |a::b::c::t->
+                triplet_inside t (resList @ [sum3 a b c])
+        |a::b::t->
+                triplet_inside t (resList @ [sum3 a b 1])
+        |a::t->
+                triplet_inside t (resList @ [sum3 a 1 1])
+    triplet_inside list List.empty
+
+
 [<EntryPoint>]
 let main argv =
-    let n = Console.ReadLine()|>Int32.Parse
+    let n = Console.ReadLine()|> Int32.Parse
     let list = readList n
-    Console.WriteLine()
-    writeList(triplet list (fun x y z-> x+y+z))
+    writeList (triplet list)
+    0
+    //writeList(triplet list (fun x y z-> x+y+z))
 
     
 
