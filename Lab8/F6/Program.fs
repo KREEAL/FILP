6bc1dd84830f5c2dfa18f6f52a3df6ab11cb42f4
Цветков Киллриал <k-real1337@mail.ru>
2022-03-23 12:08:52 +0300

    generating

--- Lab8/F6/Program.fs
+++ Lab8/F6/Program.fs
@@ -23,7 +23,7 @@ type driversLicense(nam:string, surnam:string, birtDt:DateTime, plac:string, ext
           | :? driversLicense as y -> if ((this.num) = (y.num)) then 1 else 0
           | _ -> invalidArg "yobj" "cannot compare values of different types" 
     end
-    override this.ToString() = "\nВодительские права:"+"\n Имя: "+ this.name + "\n Фамилия: " + this.surname+ "\n Дата рождения: "+ this.birthDt.ToShortDateString()  + "\n Место рождения: " + this.place+ "\n Дата выдачи: "+ this.extrDT.ToShortDateString() + "\n Дата конца срока: "+ this.exprDT.ToShortDateString() + "\n Номер: " + this.num.ToString()+"\n"
+    override this.ToString() = "\nВодительские права:"+"\n Имя: "+ this.name + "\n Фамилия: " + this.surname+ "\n Дата рождения: "+ this.birthDt.ToString()  + "\n Место рождения: " + this.place+ "\n Дата выдачи: "+ this.extrDT.ToShortDateString() + "\n Дата конца срока: "+ this.exprDT.ToShortDateString() + "\n Номер: " + this.num.ToString()+"\n"
 
 [<AbstractClass>]
 type DocClass() =
@@ -48,6 +48,7 @@ type ArrayDocClass(list:'driversLicense list)=
 
         Console.WriteLine(endTime-startTime)
         res
+    override this.ToString() = this.Arr.Length.ToString()
 
 type ListDocClass(list:'driversLicense list)=
     inherit DocClass()
@@ -126,6 +127,37 @@ type Tree=
     |Leaf of int
     |Node of Tree * Tree
 
+(*let randomStr = 
+    let chars = "ABCDEFGHIJKLMNOPQRSTUVWUXYZ"
+    let charsLen = chars.Length
+    let random = System.Random()    
+    let q = fun len -> 
+        let randomChars = [|for i in 0..len -> chars.[random.Next(charsLen)]|]
+        String(randomChars)
+    q*)
+let randomDt l = 
+    let random = System.Random()
+    let day = random.Next(1,32)
+    let month = random.Next(1,12)
+    let year = random.Next(1900,2022)
+    day.ToString()+"/"+month.ToString()+"/"+year.ToString() + " 0:00:00"
+    //month.ToString()+"/"+day.ToString()+"/"+year.ToString() + " 0:00:00"
+    DateTime.Now
+
+let randNum l =
+    let random = System.Random()
+    random.Next(1000000,10000000)
+
+let rec generator obj i =
+    if i < 30000 then
+        let newI = i + 1
+        let newDr = driversLicense("Name","Surname",randomDt 0,"Place",randomDt 0,randomDt 0,randNum 0)
+        //Console.WriteLine(newDr)
+        generator (List.append obj [newDr]) newI
+    else
+        obj
+        
+
 
 [<EntryPoint>]
 let main argv =
@@ -136,30 +168,40 @@ let main argv =
     let drL4 = driversLicense("Илья","Ильин",DateTime.Parse "04.04.1994","г.Краснодар",DateTime.Parse "04.04.2024",DateTime.Parse "02.02.2034",4444444)
     let drL5 = driversLicense("Егор","Егоров",DateTime.Parse "05.05.1995","г.Тихорецк",DateTime.Parse "05.05.2025",DateTime.Parse "02.02.2035",3333333)
     
-    let licensesList = [drL1]@[drL2]@[drL3]@[drL4]@[drL5]
+    
+
+    
+    //Console.WriteLine(generator [] )
+
+    let generatedList = generator [] 0
+
+    let licensesList = [drL1]@[drL2]@[drL3]@generatedList@[drL4]@[drL5]
+
+    
+    //Console.WriteLine(licensesList)
+    ////********************************************************//
+    //let coolArray = ArrayDocClass(licensesList)
+    //Console.WriteLine(coolArray)
+    //coolArray.searchDoc(drL4)//30к-114
+    ////********************************************************//
+
+    
+    ////********************************************************//
+    //let coolList = ListDocClass(licensesList)
+    //coolList.searchDoc(drL4)//30k - 124
+    ////********************************************************//
 
     ////********************************************************//
-    let coolArray = ArrayDocClass(licensesList)
-    coolArray.searchDoc(drL4)//где-то 50 тиков проходит(45-60)
-    ////********************************************************//
-
-
-    ////********************************************************//
-    let coolList = ListDocClass(licensesList)
-    coolList.searchDoc(drL4)//три раза подряд дало 53 тика
-    ////********************************************************//
-
-    ////********************************************************//
-    let coolSet = SetDocClass(licensesList)
-    coolSet.searchDoc(drL4)//47-77 тиков
+    //let coolSet = SetDocClass(licensesList)
+    //coolSet.searchDoc(drL4)//30k 104
     ////**************************
 
     ////********************************************************//
-    let coolBinaryList = SetDocClass(licensesList)
-    coolBinaryList.searchDoc(drL4)//56-61
+    //let coolBinaryList = SetDocClass(licensesList)
+    //coolBinaryList.searchDoc(drL4)//30k - 90
     ////***************************
 
-
+    
     //Console.WriteLine(Ar1)
     printfn "Hello World from F#!"
     0 // return an integer exit code
